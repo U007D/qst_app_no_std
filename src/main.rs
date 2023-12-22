3d2678527246fc028abee73ae1aaa7738baf9c15
Brad Gibson <u007d@humanenginuity.com>
2023-12-22 07:03:47 -0800

    enable local unit testing with `cargo test_host` alias

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-#![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
 
 mod handler;
 
 // `#[no_mangle]` is unsafe but is required to be able to call application entry point
 #[allow(unsafe_code)]
-#[no_mangle]
+#[cfg_attr(not(test), no_mangle)]
 #[allow(clippy::missing_const_for_fn)]
 extern "C" fn main() -> ! {
     #[allow(clippy::empty_loop)]
